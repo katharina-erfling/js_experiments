8b28031deb9746ec3a56a6b1972adf384b9b0a15
Katharina Erfling <katharina.erfling@googlemail.com>
2026-03-24 16:58:52 +0100

    Add First Exercises

---
+++ js/uebungen1.js
@@ -0,0 +1,53 @@
+/*Übung 1*/
+console.log('Übung 1');
+console.log('Katharina');
+console.log('..........');
+
+
+/*Übung 2*/
+alert('Alert! Your console is on fire! Hurry! Write an email to the fire department');
+    
+
+/*Übung 3*/
+console.log('Übung 3');
+console.log(2000 % 60);
+console.log('..........');
+
+/*Übung 4*/
+console.log('Übung 4');
+'use strict';
+console.log('Katharina'.length * 'Erfling'.length);
+console.log('..........');
+
+/*Übung 5*/
+'use strict';
+console.log('Übung 5');
+console.log(typeof '42');
+console.log('..........');
+
+/*Übung 6*/
+'use strict';
+console.log(3 + 4);
+console.log(3 * 12);
+console.log(25 + 12);
+console.log((12 - 3) / 3);
+console.log(12 % 3);
+console.log(12 * (44 / 11) / 3 + 67);
+console.log('..........');
+
+/*Übung 7*/
+'use strict';
+let alter = prompt('Wie alt bist du?')
+console.log('Du bist', alter, 'Jahre alt');
+console.log('..........');
+
+/* Übung 8 */
+'use strict';
+const TAX_PERCENTAGE = 22;
+let price = 150;
+
+let totalPrice = price * (1 + TAX_PERCENTAGE / 100);
+// 1 = 100% Preis, + TAX_PERCENTAGE/100 = Steueranteil (0.22) → zusammen 1.22
+
+console.log(totalPrice);
+console.log('..........');
