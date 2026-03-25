2e979d59d649e5a2759fc6f1ca806d72a878bad9
Katharina Erfling <katharina.erfling@googlemail.com>
2026-03-25 22:18:16 +0100

    extend string exercises with quotes, constants and prompt inputs

--- js/uebungen3.js
+++ js/uebungen3.js
@@ -42,10 +42,50 @@ console.log('Dein Lieblingsessen ist ' + lieblingsEssen + ` und dein Lieblingsge
 console.log('..........');
 
 
-/*Übung 5 - Fehler im Detail*/
+
+/*Übung 5 - Strings in Variablen*/
+console.log('Übung 5 - Strings in Variablen');
+let meinName = 'Katharina';
+let meineLieblingsFarbe = 'Pink';
+let meinLieblingsEssen = 'Mochi';
+
+console.log(`Mein Name ist ${meinName}, meine Lieblingsfarbe ist ${meineLieblingsFarbe} und mein Lieblingsessen ist ${meinLieblingsEssen}`);
+console.log('..........');
+
+
+
+
+/*Übung 6 - Zitate in Strings*/
+console.log('Übung 6 - Zitate in Strings');
+console.log('Meine Freundin heißt "Nina".');
+console.log("Meine Freundin heißt \"Nina\".");
+console.log('..........');
+
+
+
+
+
+/*Übung 7 - Fehler im Detail*/
 console.log('Übung 5 - Fehler im Detail');
 
 const TALE = `Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?'\n\nSo she was considering in her own mind, as well as she could, for the hot day made her feel very sleepy and stupid, whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a white rabbit with pink eyes ran close by her.\n\nThere was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the rabbit say to itself, 'Oh dear! Oh dear! I shall be too late!' (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural);\n\nBut when the rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.`;
 
 console.log(TALE);
 console.log('..........');
+
+
+
+/*Übung 8 - Konkatenation von Strings*/
+console.log('Übung 8 - Konkatenation von Strings');
+
+let vorname = 'Katharina';
+let alter = '36';
+let lieblingsFilm = `Herr Der Ringe`;
+
+console.log(`Mein Name ist ${vorname}, ich bin ${alter} Jahre alt und mein Lieblingsfilm ist ${lieblingsFilm}`);
+
+console.log('..........');
+
+
+
+
