47af9a60af5798200180fc49a4564748d886e84b
Katharina Erfling <katharina.erfling@googlemail.com>
2026-04-12 22:28:34 +0200

    Update project structure and add new directories

---
+++ dom-abfrage/dom-abfrage.js
@@ -0,0 +1,29 @@
+// Aufgabe 1
+document.querySelector('h1');
+document.querySelector('#buy_form');
+document.querySelector('#product_img');
+
+
+// Aufgabe 2
+document.querySelectorAll('li');
+document.querySelectorAll('h2');
+document.querySelectorAll('.special');
+document.querySelectorAll('li.keyword');
+document.querySelectorAll('span.special');
+document.querySelectorAll('.i.b');
+
+
+// Aufgabe 3
+document.querySelector('ul#product_specification li');
+document.querySelector('h1.article span');
+document.querySelectorAll('p .keyword');
+document.querySelectorAll('ul li');
+
+
+// Aufgabe 4
+$$('img[src$=jpg]');
+$$('form input[type=”button”]');
+$$('.model[data-model*=”V7”]');
+$$('img:not(.float_left)');
+$$('li:nth-child(2)');
+$$('h2 + ul');
---
+++ dom-test/dom-test1.js
@@ -0,0 +1,25 @@
+// Aufgabe 1: Änderung der H1
+document.querySelector('h1').innerHTML = '<em>Almost Famous Quotes</em>';
+
+
+// Aufgabe 2: Blockquote manipulieren
+document.querySelector('blockquote').innerHTML = `
+  <p>
+    I have always wished for my computer to be as easy to use as my telephone;
+    my wish has come true because I can no longer figure out how to use my
+    telephone.
+  </p>
+  <footer>— <cite>Bjarne Stroustrup</cite></footer>
+`;
+
+
+
+
+
+
+
+
+
+
+
+
