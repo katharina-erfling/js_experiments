4b9c78010d4cf7b1ca6019e03ef991d547f5b936
Katharina Erfling <katharina.erfling@googlemail.com>
2026-04-22 23:12:18 +0200

    add node exercises

--- kleine-uebungen/4_asynchrones-arbeiten/uebung13/uebung13.js
+++ kleine-uebungen/4_asynchrones-arbeiten/uebung13/uebung13.js
@@ -1,1 +1,22 @@
 const dns = require('dns')
+const IP_V = 4
+const URL = 'lilyversell.de'
+
+const getIP = (url, ip) => {
+    return new Promise((resolve, reject) => {
+        dns.lookup(url, ip, (error, data) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({address: data, family: ip});
+            }
+        });
+    });
+};
+
+getIP(URL, IP_V)
+    .then((data) => console.log(`IP address = ${data.address}`))
+    .catch((err) => console.error(err));
+
+
+
